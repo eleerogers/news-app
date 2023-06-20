4892d19414aa6ce45f7dc9b921b3dba937b56f6e
Lee Rogers <edwinleerogers@gmail.com>
2023-06-19 22:36:54 -0700

    handle/style images

--- components/NewsArticleEntry.tsx
+++ components/NewsArticleEntry.tsx
@@ -1,19 +1,25 @@
 import { NewsArticle } from "@/models/NewsArticles";
 import { Card } from 'react-bootstrap'
+import Image from 'next/image'
+import placeholderImage from "@/assets/images/news_placeholder.jpg"
+import styles from '@/styles/NewsArticleEntry.module.css'
 
 interface NewsArticleEntryProps {
   article: NewsArticle,
 }
 
 const NewsArticleEntry = ({article: {title, description, url, urlToImage}}: NewsArticleEntryProps) => {
-  const validImageUrl = urlToImage?.startsWith("http://") || urlToImage?.startsWith("https://") ? urlToImage : undefined
+  const validImageUrl = (urlToImage?.startsWith("http://") || urlToImage?.startsWith("https://")) ? urlToImage : undefined
 
   return (
     <a href={url}>
       <Card className="h-100">
-        <Card.Img
-          variant="top"
-          src={validImageUrl}
+        <Image
+          src={validImageUrl || placeholderImage}
+          width={500}
+          height={200}
+          alt="News article image"
+          className={`card-img-top ${styles.image}`}
         />
         <Card.Body>
           <Card.Title>{title}</Card.Title>
